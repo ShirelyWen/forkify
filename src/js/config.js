832d0f7c148be430250bdaa772f1916d9c858099
ShirelyWen <shirelywen@gmail.com>
2022-09-16 16:24:59 +1000

    forkify project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '9f71499a-a7e3-4891-8136-97a7b3c5f493';
+export const MODAL_CLOSE_SEC = 2.5;
